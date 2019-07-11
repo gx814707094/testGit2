df50ff198ee4da5298c265bcb79a6a4e84ee12c2
gx_glb <814707094@qq.com>
2019-07-11 11:34:57 +0800

    di yi ci ti jiao xiang mu

---
+++ src/main/java/com/bw/main/Dog.java
@@ -0,0 +1,8 @@
+package com.bw.main;
+
+public class Dog {
+	public static void main(String[] args) {
+		System.out.println("主代码的小狗");
+		
+	}
+}
---
+++ src/test/java/com/bw/test/DogTest.java
@@ -0,0 +1,8 @@
+package com.bw.test;
+
+public class DogTest {
+	public static void main(String[] args) {
+		
+		System.out.println("²âÊÔÖĞµÄĞ¡¹·");
+	}
+}
